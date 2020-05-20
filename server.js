d884bfab656b6f69d35333134d3022ff2ecb21f7
Dusan Matic <56050536+Borbiloblica@users.noreply.github.com>
2020-05-20 11:17:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var data = require('./data/test.json');
+
+
 var app = express();
 const PORT = 3000;
 
@@ -9,4 +12,5 @@ app.use(express.static(__dirname));
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data);
 });
